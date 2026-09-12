26a91d5654458eb620614c807125ba9f820b0f66
Leo Simmel <lemue00@gmail.com>
2026-09-12 13:44:39 +0200

    added conversion function text_node_to_html_node.

--- src/textnode.py
+++ src/textnode.py
@@ -1,13 +1,15 @@
 from enum import Enum
 
+from leafnode import LeafNode
+
 
 class TextType(Enum):
     PLAIN_TEXT = "text"
     BOLD_TEXT = "bold"
     ITALIC_TEXT = "italic"
     CODE_TEXT = "code"
-    LINKS = "link"
-    IMAGES = "image"
+    LINK = "link"
+    IMAGE = "image"
 
 class TextNode:
     def __init__(self, text: str, text_type: TextType, url: str | None = None):
@@ -26,3 +28,24 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    match(text_node.text_type):
+        case TextType.PLAIN_TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD_TEXT:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC_TEXT:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE_TEXT:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            if not text_node.url:
+                raise ValueError("link doesn't have URL")
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            if not text_node.url:
+                raise ValueError("image doesn't have URL")
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise ValueError(f"invalid text type: {text_node.text_type}")
