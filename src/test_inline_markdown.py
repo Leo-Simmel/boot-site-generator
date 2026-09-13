6992c1d5b161e40070db37086697fa3290c6614f
Leo Simmel <lemue00@gmail.com>
2026-09-13 23:13:27 +0200

    added another test.

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -420,40 +420,40 @@ class TestInlineMarkdown(unittest.TestCase):
 
 
 # text to textnodes
-def test_text_to_textnodes(self):
-        nodes = text_to_textnodes(
-            "This is **text** with an _italic_ word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
-        )
-        self.assertListEqual(
-            [
-                TextNode("This is ", TextType.TEXT),
-                TextNode("text", TextType.BOLD),
-                TextNode(" with an ", TextType.TEXT),
-                TextNode("italic", TextType.ITALIC),
-                TextNode(" word and a ", TextType.TEXT),
-                TextNode("code block", TextType.CODE),
-                TextNode(" and an ", TextType.TEXT),
-                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
-                TextNode(" and a ", TextType.TEXT),
-                TextNode("link", TextType.LINK, "https://boot.dev"),
-            ],
-            nodes,
-        )
+    def test_text_to_textnodes(self):
+            nodes = text_to_textnodes(
+                "This is **text** with an _italic_ word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+            )
+            self.assertListEqual(
+                [
+                    TextNode("This is ", TextType.TEXT),
+                    TextNode("text", TextType.BOLD),
+                    TextNode(" with an ", TextType.TEXT),
+                    TextNode("italic", TextType.ITALIC),
+                    TextNode(" word and a ", TextType.TEXT),
+                    TextNode("code block", TextType.CODE),
+                    TextNode(" and an ", TextType.TEXT),
+                    TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                    TextNode(" and a ", TextType.TEXT),
+                    TextNode("link", TextType.LINK, "https://boot.dev"),
+                ],
+                nodes,
+            )
 
-def test_to_textnodes_without_space(self):
-        nodes = text_to_textnodes(
-            "**text**_italic_`code block`![image](https://i.imgur.com/zjjcJKZ.png)[link](https://boot.dev)"
-        )
-        self.assertListEqual(
-            [
-                TextNode("text", TextType.BOLD),
-                TextNode("italic", TextType.ITALIC),
-                TextNode("code block", TextType.CODE),
-                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
-                TextNode("link", TextType.LINK, "https://boot.dev"),
-            ],
-            nodes,
-        )
+    def test_to_textnodes_without_space(self):
+            nodes = text_to_textnodes(
+                "**text**_italic_`code block`![image](https://i.imgur.com/zjjcJKZ.png)[link](https://boot.dev)"
+            )
+            self.assertListEqual(
+                [
+                    TextNode("text", TextType.BOLD),
+                    TextNode("italic", TextType.ITALIC),
+                    TextNode("code block", TextType.CODE),
+                    TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                    TextNode("link", TextType.LINK, "https://boot.dev"),
+                ],
+                nodes,
+            )
 
 if __name__ == "__main__":
     unittest.main()
